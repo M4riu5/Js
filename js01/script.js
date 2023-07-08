48492dacb802cf3aaae87d54628c58e589866528
Marius <mariushaf@gmail.com>
2023-07-08 13:32:20 +0300

    Learn

---
+++ js01/script.js
@@ -0,0 +1,31 @@
+// Data types 
+String = "hello"
+Number = 12
+Boolean = true // false
+// ARRAY
+const cars = ["BMW" , 12 , true , false , ['bmw' , 23]]
+
+// Object
+const person =  {
+    name: "John",  
+    surname: "Doe",
+    number: 1231231,
+}
+// Math
+const sudet = 2+2
+console.log('sudet -->', sudet);
+
+const atimti = 5-3
+console.log('atimti -->', atimti);
+
+const dalinti = 10/2
+console.log('dalinti -->', dalinti);
+
+const test = "Iphone" + 12
+console.log('test -->', test);
+const testone = 'Iphone' + "2"
+console.log('testone -->', testone);
+const testtwo = "Js" + 2 + "test"
+console.log('testtwo -->', testtwo);
+const testtree = "Js"
+console.log('testtree -->',`hello my name is ${testtree} , number =  ${2+5}` );
